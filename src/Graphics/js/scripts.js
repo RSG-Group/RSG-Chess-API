4a0863fbcd74b17b7f1b981b275029b7a8240dfa
Radi Cholakov <radicho123@gmail.com>
2018-04-02 12:07:02 +0300

    Complete the Graphics ChessTable component (without styles).

--- src/Graphics/js/scripts.js
+++ src/Graphics/js/scripts.js
@@ -3,122 +3,56 @@
 // Licensed under Apache 2.0 LICENSE
 
 import React from 'react'
-import _ from 'lodash'
+import PropTypes from 'prop-types'
+import { omit, find } from 'lodash'
 import classNames from 'classnames'
 
-export default class MainComponent extends React.Component {
-  constructor () {
-    super()
-
-    this.state = {
-      selected: null,
-      promotionParams: null,
-      checkmate: null,
-      welcomeDialog: true,
-      settingsDialog: false,
-      playAgainstAI: false,
-      rotated: false,
-      showValidMoves: true
-    }
-  }
-
-  __handleReplay () {
-    // Set state to null and false, to reset all params
-    this.setState({
-      selected: null,
-      promotionParams: null,
-      welcomeDialog: true,
-      checkmate: null,
-      settingsDialog: false,
-      playAgainstAI: false
-    })
-    // Initialize new game
-    this.props.initializeGame()
-  }
-
-  __handleClick (x, y) {
-    const { game } = this.props
-
-    var selected = this.state.selected
-    if (selected) {
-      game.moveSelected(
-        selected, {x: x, y: y}, this.__handlePromotion, this.__handleCheckmate, this.state.playAgainstAI, false
-      )
-      this.setState({ selected: null })
-    } else {
-      var last = game.turn.length - 1
-      if (
-        game.board[y][x] &&
-        (last >= 0 ? game.board[y][x].color !== game.turn[last].color
-          : game.board[y][x].color === 'W')
-      ) {
-        this.setState({ selected: game.board[y][x] })
-      } else {
-        game.board[y][x] && alert('Invalid Move!')
-      }
-    }
-  }
-
-  __handlePromotion (pawn, x, y, color) {
-    this.setState({
-      promotionParams: {
-        x: x,
-        y: y,
-        color: color,
-        pawn: pawn
-      }
-    })
-  }
-
-  __handleCheckmate (color) {
-    this.setState({ checkmate: color })
-  }
-
-  __handleGamePromotion (piece) {
-    if (piece) {
-      const { x, y, color, pawn } = this.state.promotionParams
-      this.props.game.promotePawn(pawn, x, y, color, piece)
-    }
-    this.setState({ promotionParams: null })
-  }
-
-  __renderTable () {
-    const { selected, rotated, showValidMoves } = this.state
-    const validMoves = selected && selected.getValidMoves(true)
-    return this.props.game.board.map((rank, i) => (
-      <tr key={i}>
-        {
-          rank.map((piece, j) => (
-            <td key={j}
-              onClick={this.__handleClick.bind(this, j, i)}
-              className={classNames({
-                selected: selected && selected === piece,
-                validMoves: showValidMoves && selected && _.find(validMoves, { x: j, y: i }),
-                rotated: rotated && piece && piece.color === 'B'
-              })}
-            >
-              {piece && piece.char}
-            </td>
-          ))
-        }
-      </tr>
-    ))
-  }
-
-  render () {
-    return (
-      <div>
-        <table id={'table'} >
-          <tbody>
-            {this.__renderTable()}
-          </tbody>
-        </table>
-
-        { this.state.promotionParams && this.props.promotion() }
-        { this.state.settingsDialog && this.props.settings() }
-        { this.state.checkmate && this.props.checkmate() }
-        { this.props.welcome() }
-      </div>
-    )
-  }
+const ChessTable = (props) => {
+  const { board, selected, rotated, showValidMoves, onClick, self } = props
+  const validMoves = selected && selected.getValidMoves(true)
+  return (
+    <div>
+      <table className="rsg-chess-table" {...omit(props, ['board', 'selected', 'rotated', 'showValidMoves', 'onClick', 'self'])}>
+        <tbody>
+          {
+            board.map((rank, i) => (
+              <tr key={i}>
+                {
+                  rank.map((piece, j) => (
+                    <td key={j}
+                      onClick={onClick.bind(self, j, i)}
+                      className={classNames({
+                        selected: selected && selected === piece,
+                        validMoves: showValidMoves && selected && find(validMoves, { x: j, y: i }),
+                        rotated: rotated && piece && piece.color === 'B'
+                      })}
+                    >
+                      {piece && piece.char}
+                    </td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
 }
+
+ChessTable.propTypes = {
+  board: PropTypes.array,
+  selected: PropTypes.object,
+  rotated: PropTypes.bool,
+  showValidMoves: PropTypes.bool,
+  onClick: PropTypes.func,
+  self: PropTypes.any
+}
+
+ChessTable.defaultProps = {
+  selected: null,
+  rotated: false,
+  showValidMoves: false
+}
+
+export default ChessTable
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 // Let's import...
 import Game from './game'
-import * as Piece from './pieces'
+import * as Pieces from './pieces'
 import AI from './AI'
-
-const Graphics = 'This feature is coming soon!'
 
 // And export...
 export {
   Game,
-  Piece,
-  AI,
-  Graphics
+  Pieces,
+  AI
 }
