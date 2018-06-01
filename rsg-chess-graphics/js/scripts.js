6e19ef0ccd4b918fa524f1b8904a22fcf69eb368
Radi Cholakov <radicho123@gmail.com>
2018-06-01 21:41:44 +0300

    Rotate only the piece char instead of the entire cell.

--- rsg-chess-graphics/js/scripts.js
+++ rsg-chess-graphics/js/scripts.js
@@ -2,125 +2,140 @@
 // RSG Chess
 // Licensed under Apache 2.0 LICENSE
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import { omit, find } from 'lodash'
-import classNames from 'classnames'
+import React from "react";
+import PropTypes from "prop-types";
+import { omit, find } from "lodash";
+import classNames from "classnames";
 
-const ChessTable = (props) => {
+const ChessTable = props => {
   // Define needed props and vars
-  const { board, selected, rotated, showValidMoves, onClick, self } = props
-  const validMoves = selected && selected.getValidMoves(true)
+  const { board, selected, rotated, showValidMoves, onClick, self } = props;
+  const validMoves = selected && selected.getValidMoves(true);
 
   // Styles variables
   const omitProps = [
-    'style',
-    'board',
-    'selected',
-    'rotated',
-    'showValidMoves',
-    'onClick',
-    'self',
-    'validBG',
-    'selectedBG',
-    'selectedColor',
-    'whiteCells',
-    'blackCells',
-    'boardWidth',
-    'boardHeight',
-    'pieceSize',
-    'tdTransition',
-    'whiteColor',
-    'blackColor'
-  ]
+    "style",
+    "board",
+    "selected",
+    "rotated",
+    "showValidMoves",
+    "onClick",
+    "self",
+    "validBG",
+    "selectedBG",
+    "selectedColor",
+    "whiteCells",
+    "blackCells",
+    "boardWidth",
+    "boardHeight",
+    "pieceSize",
+    "tdTransition",
+    "whiteColor",
+    "blackColor"
+  ];
 
   const mainTable = {
-    cursor: 'pointer',
-    border: '1px solid gray',
-    textAlign: 'center',
+    cursor: "pointer",
+    border: "1px solid gray",
+    textAlign: "center",
     width: props.boardWidth,
     height: props.boardHeight,
     fontSize: props.pieceSize,
-    userSelect: 'none',
-    WebkitTouchCallout: 'none',
-    WebkitUserSelect: 'none',
-    KhtmlUserSelect: 'none',
-    MozUserSelect: 'none',
-    msUserSelect: 'none',
+    userSelect: "none",
+    WebkitTouchCallout: "none",
+    WebkitUserSelect: "none",
+    KhtmlUserSelect: "none",
+    MozUserSelect: "none",
+    msUserSelect: "none",
     ...props.style
-  }
+  };
 
   const mainTd = {
-    width: '12.5%',
-    height: '12.5%',
-    border: '1px solid grey',
+    width: "12.5%",
+    height: "12.5%",
+    border: "1px solid grey",
     WebkitTransition: props.tdTransition,
     MozTransition: props.tdTransition,
     OTransition: props.tdTransition,
     msTransition: props.tdTransition,
     transition: props.tdTransition
-  }
+  };
 
   const rotatedStyles = {
-    transform: 'rotateX(180deg)'
-  }
+    transform: "rotateX(180deg)"
+  };
 
   const validMoveStyles = {
     background: props.validBG
-  }
+  };
 
   const selectedStyles = {
     background: props.selectedBG,
     color: props.selectedColor
-  }
+  };
 
   const whiteTds = {
     background: props.whiteCells,
     color: props.whiteColor
-  }
+  };
 
   const blackTds = {
     background: props.blackCells,
     color: props.blackColor
-  }
+  };
 
   // render graphics
   return (
-    <table className="rsg-chess-table" style={mainTable} {...omit(props, omitProps)}>
+    <table
+      className="rsg-chess-table"
+      style={mainTable}
+      {...omit(props, omitProps)}
+    >
       <tbody>
-        {
-          board.map((rank, i) => (
-            <tr key={i}>
-              {
-                rank.map((piece, j) => (
-                  <td key={j}
-                    onClick={onClick.bind(self, j, i)}
-                    className={classNames({
-                      selected: selected && selected === piece,
-                      validMoves: showValidMoves && selected && find(validMoves, { x: j, y: i }),
-                      rotated: rotated && piece && piece.color === 'B'
-                    })}
-
-                    style={{
-                      ...((i % 2 === 0 && j % 2 !== 0) || (i % 2 !== 0 && j % 2 === 0)) && blackTds,
-                      ...((i % 2 === 0 && j % 2 === 0) || (i % 2 !== 0 && j % 2 !== 0)) && whiteTds,
-                      ...mainTd,
-                      ...rotated && piece && piece.color === 'B' && rotatedStyles,
-                      ...showValidMoves && selected && find(validMoves, { x: j, y: i }) && validMoveStyles,
-                      ...selected && selected === piece && selectedStyles
-                    }}
-                  >
-                    {piece && piece.char}
-                  </td>
-                ))
-              }
-            </tr>
-          ))
-        }
+        {board.map((rank, i) => (
+          <tr key={i}>
+            {rank.map((piece, j) => (
+              <td
+                key={j}
+                onClick={onClick.bind(self, j, i)}
+                className={classNames({
+                  selected: selected && selected === piece,
+                  validMoves:
+                    showValidMoves &&
+                    selected &&
+                    find(validMoves, { x: j, y: i }),
+                  rotated: rotated && piece && piece.color === "B"
+                })}
+                style={{
+                  ...(((i % 2 === 0 && j % 2 !== 0) ||
+                    (i % 2 !== 0 && j % 2 === 0)) &&
+                    blackTds),
+                  ...(((i % 2 === 0 && j % 2 === 0) ||
+                    (i % 2 !== 0 && j % 2 !== 0)) &&
+                    whiteTds),
+                  ...mainTd,
+                  ...(showValidMoves &&
+                    selected &&
+                    find(validMoves, { x: j, y: i }) &&
+                    validMoveStyles),
+                  ...(selected && selected === piece && selectedStyles)
+                }}
+              >
+                <span
+                  style={
+                    rotated && piece && piece.color === "B" && rotatedStyles
+                  }
+                >
+                  {piece && piece.char}
+                </span>
+              </td>
+            ))}
+          </tr>
+        ))}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
 // validate props
 ChessTable.propTypes = {
@@ -141,24 +156,24 @@ ChessTable.propTypes = {
   tdTransition: PropTypes.string,
   whiteColor: PropTypes.string,
   blackColor: PropTypes.string
-}
+};
 
 // set default props
 ChessTable.defaultProps = {
   selected: null,
   rotated: false,
   showValidMoves: false,
-  validBG: 'red',
-  selectedBG: 'brown',
-  selectedColor: 'lightblue',
-  whiteCells: 'rgb(255, 205, 160)',
-  blackCells: 'rgb(210, 140, 70)',
-  boardWidth: '500px',
-  boardHeight: '500px',
-  pieceSize: '40px',
-  tdTransition: 'background-color 355ms linear, color 360ms ease-in',
-  whiteColor: 'black',
-  blackColor: 'black'
-}
+  validBG: "red",
+  selectedBG: "brown",
+  selectedColor: "lightblue",
+  whiteCells: "rgb(255, 205, 160)",
+  blackCells: "rgb(210, 140, 70)",
+  boardWidth: "500px",
+  boardHeight: "500px",
+  pieceSize: "40px",
+  tdTransition: "background-color 355ms linear, color 360ms ease-in",
+  whiteColor: "black",
+  blackColor: "black"
+};
 
-export default ChessTable
+export default ChessTable;
