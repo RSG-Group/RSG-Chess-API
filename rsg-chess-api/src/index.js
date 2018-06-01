a6138c5361cbff820f05be52c2065580c6edb49a
Radi Cholakov <radicho123@gmail.com>
2018-06-01 21:24:50 +0300

    Add API tools.

--- rsg-chess-api/src/index.js
+++ rsg-chess-api/src/index.js
@@ -1,11 +1,13 @@
 // Let's import...
 import Game from './game'
 import * as Pieces from './pieces'
+import * as tools from './tools'
 import AI from './AI'
 
 // And export...
 export {
   Game,
   Pieces,
-  AI
+  AI,
+  tools
 }
